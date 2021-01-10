6e9b9f7ab1847676bbdcb7fbd77f4648874cce3a
blowhacker <n@vain.in>
2021-01-10 14:03:15 +0000

    fix on range in rust and python

--- rust/prime.rs
+++ rust/prime.rs
@@ -5,7 +5,7 @@ fn main() {
         let mut prime = 1;
         let sqr = (n as f64).sqrt() as i32;
         
-        for i in (3..sqr).step_by(2){
+        for i in (3..sqr +1 ).step_by(2){
             if n%i == 0 {
                 prime = 0;
                 break;
