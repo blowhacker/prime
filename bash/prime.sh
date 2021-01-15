dfefa3ed9fb46907b6799a99a4e67314a744637a
blowhacker <n@vain.in>
2021-01-15 23:33:53 +0000

    bash bug fix

--- bash/prime.sh
+++ bash/prime.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-max=1000000
+max=100
 
 for ((num=3;num<max;num=num+2))
 do
-echo $num
     prime=1
-    sqr=$(echo "sqrt($num)" | bc)
+    sqr=$(echo "sqrt($num)+1" | bc)
 
     for ((i=3;i<sqr;i=i+2))
-    do
+    do    
         rem=$(($num%$i))
-        if [ rem = 0 ]
-        then
-            prime=0
-            break
-        fi
+
+    if [[ $rem -lt 1 ]]; then
+        prime=0
+        break
+    fi
     done
 
-    if [ prime = 1 ]
-    then
+    if [[ $prime -gt 0 ]]; then    
         echo $num
     fi
 
