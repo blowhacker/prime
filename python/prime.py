d85238ea06178f9a553e53f79cd1ed36031b133a
Kumar Navin <kumar.navin@bcgplatinion.com>
2021-03-28 12:19:15 +0100

    added readme.md + python3 fix

--- python/prime.py
+++ python/prime.py
@@ -5,7 +5,7 @@ max = 1000000
 for num in range (3, max, 2) :
     prime = 1
     sqrt = int(math.sqrt(num))
-    for i in xrange(3, sqrt + 1, 2):
+    for i in range(3, sqrt + 1, 2):
         if num%i == 0:
             prime =0
             break
